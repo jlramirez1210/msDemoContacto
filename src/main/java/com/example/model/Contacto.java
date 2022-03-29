35774c1e3777fb55da985aa4f64e7c73c48dca7a
Jorge <jlramirez1210@gmail.com>
2022-03-29 00:00:37 -0500

    Creacion de proyecto e entidad contacto

---
+++ src/main/java/com/example/model/Contacto.java
@@ -0,0 +1,58 @@
+package com.example.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contactos")
+public class Contacto implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idContacto;
+
+	private int edad;
+	private String email;
+	private String nombre;
+
+	public Contacto() {
+		super();
+	}
+
+	public int getIdContacto() {
+		return idContacto;
+	}
+
+	public void setIdContacto(int idContacto) {
+		this.idContacto = idContacto;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
